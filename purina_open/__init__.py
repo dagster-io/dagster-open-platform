959bb2a7d40bb054064eb207664237d04da82f65
elementl-devtools <devtools@elementl.com>
2023-09-12 19:55:42 +0000

    Mirroring commit d565783aeb3da308414835ccb1960d2821475906 from internal

--- purina_open/__init__.py
+++ purina_open/__init__.py
@@ -1,9 +1,15 @@
 from dagster import Definitions, load_assets_from_modules
 
 from .assets import health_check
+from .resources.dbt_resource import dbt_resource
+from .resources.stitch_resource import stitch_resource
 
 all_assets = load_assets_from_modules([health_check])
 
 defs = Definitions(
     assets=all_assets,
+    resources={
+        "dbt": dbt_resource,
+        "stitch": stitch_resource,
+    },
 )
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="purina_open",
     packages=find_packages(exclude=["purina_open_tests"]),
-    install_requires=["dagster", "dagster-cloud"],
+    install_requires=["dagster", "dagster-cloud", "dagster-dbt", "dbt-snowflake"],
     extras_require={
         "dev": ["dagster-webserver", "pytest"],
         "tests": ["pytest"],
