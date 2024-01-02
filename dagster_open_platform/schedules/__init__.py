b7a241f112970395fced6b832422fd9dd5f58e27
elementl-devtools <devtools@elementl.com>
2024-01-02 22:12:58 +0000

    Mirroring commit bc755fb86f2b3eef48ab3f8c270e93da083b02a0 from internal

--- dagster_open_platform/schedules/__init__.py
+++ dagster_open_platform/schedules/__init__.py
@@ -1,8 +1,10 @@
 from dagster import (
     AssetSelection,
+    RunRequest,
     ScheduleDefinition,
     build_schedule_from_partitioned_job,
     define_asset_job,
+    schedule,
 )
 from dagster_dbt import build_dbt_asset_selection
 
@@ -29,6 +31,39 @@ insights_job = define_asset_job(
 
 insights_schedule = build_schedule_from_partitioned_job(job=insights_job)
 
-scheduled_jobs = [oss_telemetry_job, insights_job]
 
-schedules = [oss_telemetry_schedule, insights_schedule]
+assets_dependent_on_cloud_usage = [
+    ["postgres", "usage_metrics_daily_jobs_aggregated"],
+    ["hightouch_usage_metrics_daily"],
+]
+
+cloud_usage_metrics_selection = None
+
+# This loops gets all upstream assets for the leaf node assets that depend
+# on cloud analytics and unions them together to get a selection
+for dependent_asset in assets_dependent_on_cloud_usage:
+    curr_selection = (
+        AssetSelection.keys(dependent_asset).upstream().required_multi_asset_neighbors()
+    )
+    if cloud_usage_metrics_selection is None:
+        cloud_usage_metrics_selection = curr_selection
+    else:
+        cloud_usage_metrics_selection = cloud_usage_metrics_selection | curr_selection
+
+cloud_usage_metrics_job = define_asset_job(
+    name="cloud_usage_metrics_job", selection=cloud_usage_metrics_selection
+)
+
+
+# Cloud usage metrics isn't partitioned, but it uses a partitioned asset
+# that is managed by Insights. It doesn't matter which partition runs
+# but does need to specify the most recent partition of Insights will be run
+@schedule(cron_schedule="0 3 * * *", job=cloud_usage_metrics_job)
+def cloud_usage_metrics_schedule():
+    most_recent_partition = insights_partition.get_last_partition_key()
+    yield RunRequest(partition_key=str(most_recent_partition), run_key=str(most_recent_partition))
+
+
+scheduled_jobs = [oss_telemetry_job, insights_job, cloud_usage_metrics_job]
+
+schedules = [oss_telemetry_schedule, insights_schedule, cloud_usage_metrics_schedule]
