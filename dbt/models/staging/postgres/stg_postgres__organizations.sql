67c57dfebb4c534c2c7d6bfbb1e85a79f852ed35
elementl-devtools <devtools@elementl.com>
2024-01-04 21:56:59 +0000

    Mirroring commit a161675e2f96461c9959f7f8ca17f03917e31a66 from internal

--- dbt/models/staging/postgres/stg_postgres__organizations.sql
+++ dbt/models/staging/postgres/stg_postgres__organizations.sql
@@ -7,7 +7,10 @@ select
 
     internal as is_internal,
     status, -- can be null, what does that mean?
-    plan_type,
+
+    -- Team_V2 is a new plan type that has capped seats, but for reporting
+    -- purposes we want to treat it as a team plan
+    case when plan_type = 'TEAM_V2' then 'TEAM' else plan_type end as plan_type,
 
     parse_json(organization_metadata) as organization_metadata,
     parse_json(organization_settings) as organization_settings,
