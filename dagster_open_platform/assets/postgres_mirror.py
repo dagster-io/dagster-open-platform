7efecda380b246012503f14feb45d06162fab9a5
elementl-devtools <devtools@elementl.com>
2024-02-01 23:13:13 +0000

    Mirroring commit 615ffe855e5fe2d13d7dff6b7ff6cfac9228506c from internal

--- dagster_open_platform/assets/postgres_mirror.py
+++ dagster_open_platform/assets/postgres_mirror.py
@@ -1,12 +1,15 @@
 import re
+from datetime import datetime, timedelta
 
 from dagster import (
     AssetExecutionContext,
     AssetKey,
     AssetsDefinition,
+    Config,
     StaticPartitionsDefinition,
     asset,
 )
+from pydantic import Field
 
 from ..resources.sling_resource import (
     SlingMode,
@@ -16,10 +19,10 @@ from ..resources.sling_resource import (
 
 # Split into many static partitions in order to avoid long-running queries that are canceled by
 # the read replica when it syncs w/ the primary - smaller queries are less likely to be canceled
-REPO_LOCATION_DATA_NUM_CHUNKS = 10
+REPO_LOCATION_DATA_NUM_CHUNKS = 5
 
 SOURCE_STREAM = (
-    "select id, organization_id, deployment_id, create_timestamp, update_timestamp,"
+    "select id, organization_id, deployment_id, create_timestamp, timestamp as update_timestamp,"
     " jsonb_path_query_array("
     "load_repositories_response::jsonb,"
     " '$.repository_datas[0].external_repository_data.external_asset_graph_data[*].asset_key'"
@@ -28,12 +31,19 @@ SOURCE_STREAM = (
     "load_repositories_response::jsonb,"
     " '$.repository_datas[0].external_repository_data.external_asset_graph_data[*].group_name'"
     ") as group_names from repository_locations_data"
-    f" where id % {REPO_LOCATION_DATA_NUM_CHUNKS} = "
+    " where timestamp > '{lookback_timestamp}'"
+    f" and id % {REPO_LOCATION_DATA_NUM_CHUNKS} = "
 )
 
 REPO_LOCATION_DATA_CHUNKED = StaticPartitionsDefinition(
     [str(x) for x in range(REPO_LOCATION_DATA_NUM_CHUNKS)]
 )
+
+
+class SyncRepoLocationDataConfig(Config):
+    lookback_days: int = Field(
+        default=1, description="Number of days of repo location updates to sync."
+    )
 
 
 def define_sync_repo_location_data_asset(
@@ -47,15 +57,21 @@ def define_sync_repo_location_data_asset(
     )
     def sync_repo_location_data(
         context: AssetExecutionContext,
+        config: SyncRepoLocationDataConfig,
     ) -> None:
         sling: SlingResource = getattr(context.resources, sling_resource_key)
 
         for partition_key in REPO_LOCATION_DATA_CHUNKED.get_partition_keys_in_range(
             context.partition_key_range
         ):
+            # we sync data from 1 day ago to give us a time buffer
+            lookback_timestamp = (datetime.now() - timedelta(days=config.lookback_days)).strftime(
+                "%Y-%m-%d"
+            )
+
             context.log.info("Syncing partition: %s", partition_key)
             for stdout_line in sling.sync_postgres_to_snowflake(
-                source_table=f"{SOURCE_STREAM} {partition_key}",
+                source_table=f"{SOURCE_STREAM.format(lookback_timestamp=lookback_timestamp)} {partition_key}",
                 dest_table="postgres_mirror.repository_locations_data",
                 primary_key=["id"],
                 mode=SlingMode.INCREMENTAL,
