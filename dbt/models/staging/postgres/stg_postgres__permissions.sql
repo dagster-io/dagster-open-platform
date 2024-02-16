0402bd7871a4d25f68398e45493c71a1b7b1445f
elementl-devtools <devtools@elementl.com>
2024-02-16 17:32:12 +0000

    Mirroring commit 55e11cffd596ac5589923a69d66e6681948e2b76 from internal

--- dbt/models/staging/postgres/stg_postgres__permissions.sql
+++ dbt/models/staging/postgres/stg_postgres__permissions.sql
@@ -4,7 +4,11 @@ select
     organization_id,
     deployment_id,
     "GRANT" as permission_grant,
-
+    replace(
+        json_extract_path_text(
+            scope, 'deployment_scope.__enum__'
+        ), 'PermissionDeploymentScope.', ''
+    ) as scope, -- Pull the scope of the grant out of the json "scope" field.
     create_timestamp as created_at
 
 
