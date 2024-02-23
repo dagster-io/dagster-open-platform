e12a2e959e22d19feb900236739a25db090e54d9
elementl-devtools <devtools@elementl.com>
2024-02-23 20:56:06 +0000

    Mirroring commit 68c6314ca9536862f2ffee61d5f524ba98bef8a1 from internal

--- dbt/models/staging/postgres/stg_insights_snowflake_cost_submissions.sql
+++ dbt/models/staging/postgres/stg_insights_snowflake_cost_submissions.sql
@@ -5,9 +5,24 @@ select
     opaque_id,
     metric_name,
     query_id,
-    max_by(cost, last_updated) as snowflake_cost,
+    max_by(snowflake_cost, last_updated) as snowflake_cost,
     max(last_updated) as last_updated
 
-from {{ source('purina_staging', 'insights_metrics_submissions') }}
+from (
+    -- Add up costs within an individual submission file
+    select
+        organization_id,
+        deployment_id,
+        opaque_id,
+        metric_name,
+        query_id,
+        submission_file_name,
+        sum(cost) as snowflake_cost,
+        max(last_updated) as last_updated
+
+    from {{ source('purina_staging', 'insights_metrics_submissions') }}
+
+    group by 1, 2, 3, 4, 5, 6
+)
 
 group by 1, 2, 3, 4, 5
