2f0364fb645e6019243101baca7224ec4c540698
elementl-devtools <devtools@elementl.com>
2024-04-01 20:56:21 +0000

    Mirroring commit 4d35294c98da6fa16afe6743504d9d170814aa69 from internal

--- dagster_open_platform/assets/dbt.py
+++ dagster_open_platform/assets/dbt.py
@@ -1,17 +1,52 @@
 import json
+import os
+from typing import Any, Mapping, Optional
 
-from dagster import AssetExecutionContext, BackfillPolicy
+from dagster import AssetExecutionContext, AssetKey, BackfillPolicy, Config, MetadataValue
 from dagster_cloud.dagster_insights import dbt_with_snowflake_insights
-from dagster_dbt import dbt_assets
+from dagster_dbt import DagsterDbtTranslator, dbt_assets
 
 from ..partitions import insights_partition
 from ..resources import (
     DBT_MANIFEST_PATH,
     dbt_resource,
 )
-from ..utils.dbt_helpers import CustomDagsterDbtTranslator, DbtConfig
+
+SNOWFLAKE_ACCOUNT_BASE, *_ = os.getenv("SNOWFLAKE_ACCOUNT", ".").split(".")
+PURINA_DATABASE_NAME = (
+    f"PURINA_CLONE_{os.environ['DAGSTER_CLOUD_PULL_REQUEST_ID']}"
+    if os.getenv("DAGSTER_CLOUD_IS_BRANCH_DEPLOYMENT") == "1"
+    else "PURINA"
+)
+SNOWFLAKE_URL = f"https://app.snowflake.com/ax61354/{SNOWFLAKE_ACCOUNT_BASE}/#/data/databases/{PURINA_DATABASE_NAME}/schemas"
 
 INSIGHTS_SELECTOR = "+tag:insights,config.materialized:incremental"
+
+
+class CustomDagsterDbtTranslator(DagsterDbtTranslator):
+    def get_group_name(self, dbt_resource_props: Mapping[str, Any]) -> Optional[str]:
+        # Same logic that sets the custom schema in macros/get_custom_schema.sql
+        asset_path = dbt_resource_props["fqn"][1:-1]
+        if asset_path:
+            return "_".join(asset_path)
+        return "default"
+
+    def get_asset_key(self, dbt_resource_props: Mapping[str, Any]) -> AssetKey:
+        resource_database = dbt_resource_props["database"]
+        resource_schema = dbt_resource_props["schema"]
+        resource_name = dbt_resource_props["name"]
+
+        return AssetKey([resource_database, resource_schema, resource_name])
+
+    def get_metadata(self, dbt_node_info: Mapping[str, Any]) -> Mapping[str, Any]:
+        if dbt_node_info["resource_type"] != "model":
+            return {}
+
+        return {
+            "url": MetadataValue.url(
+                f"{SNOWFLAKE_URL}/{dbt_node_info['schema'].upper()}/table/{dbt_node_info['name'].upper()}"
+            )
+        }
 
 
 @dbt_assets(
@@ -22,6 +57,10 @@ INSIGHTS_SELECTOR = "+tag:insights,config.materialized:incremental"
 )
 def cloud_analytics_dbt_assets(context: AssetExecutionContext):
     yield from dbt_with_snowflake_insights(context, dbt_resource.cli(["build"], context=context))
+
+
+class DbtConfig(Config):
+    full_refresh: bool = False
 
 
 @dbt_assets(
