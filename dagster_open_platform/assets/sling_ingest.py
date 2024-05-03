9dd725c1c1a8b98100b338d96de38a8f838eb00c
elementl-devtools <devtools@elementl.com>
2024-05-03 02:19:08 +0000

    Mirroring commit 5efd8130fa38cb3d2e7cd693269121d134ffce13 from internal

--- dagster_open_platform/assets/sling_ingest.py
+++ dagster_open_platform/assets/sling_ingest.py
@@ -1,7 +1,12 @@
 from datetime import timedelta
 from pathlib import Path
 
-from dagster import AssetKey, build_last_update_freshness_checks, build_sensor_for_freshness_checks
+from dagster import (
+    AssetKey,
+    SourceAsset,
+    build_last_update_freshness_checks,
+    build_sensor_for_freshness_checks,
+)
 from dagster_embedded_elt.sling.asset_decorator import sling_assets
 from dagster_embedded_elt.sling.dagster_sling_translator import DagsterSlingTranslator
 from dagster_embedded_elt.sling.resources import (
@@ -11,25 +16,78 @@ from dagster_embedded_elt.sling.resources import (
 config_dir = Path(__file__).parent.parent / "configs" / "sling" / "cloud_product"
 
 
-class CustomSlingLowVolumeTranslator(DagsterSlingTranslator):
+class CustomSlingTranslatorMain(DagsterSlingTranslator):
     def get_group_name(self, stream_definition):
-        return "cloud_product_low_volume_ingest"
+        return "cloud_product_main"
+
+    def get_deps_asset_key(self, stream_definition):
+        stream_asset_key = super().get_deps_asset_key(stream_definition)[0]
+        return AssetKey(["main", *stream_asset_key[0]])
 
 
 @sling_assets(
     replication_config=config_dir / "main_low_volume.yaml",
-    dagster_sling_translator=CustomSlingLowVolumeTranslator(),
+    dagster_sling_translator=CustomSlingTranslatorMain(),
 )
 def cloud_product_main_low_volume(context, embedded_elt: SlingResource):
     yield from embedded_elt.replicate(context=context)
 
 
 @sling_assets(
+    replication_config=config_dir / "main_high_volume.yaml",
+    dagster_sling_translator=CustomSlingTranslatorMain(),
+)
+def cloud_product_main_high_volume(context, embedded_elt: SlingResource):
+    yield from embedded_elt.repli
+
+
+cloud_product_main_source_assets = [
+    *[
+        SourceAsset(key, group_name="postgres_main")
+        for key in cloud_product_main_low_volume.dependency_keys
+    ],
+    *[
+        SourceAsset(key, group_name="postgres_main")
+        for key in cloud_product_main_high_volume.dependency_keys
+    ],
+]
+
+
+class CustomSlingTranslatorShard1(DagsterSlingTranslator):
+    def get_group_name(self, stream_definition):
+        return "cloud_product_shard1"
+
+    def get_deps_asset_key(self, stream_definition):
+        stream_asset_key = super().get_deps_asset_key(stream_definition)[0]
+        return AssetKey(["shard1", *stream_asset_key[0]])
+
+
+@sling_assets(
     replication_config=config_dir / "shard1_low_volume.yaml",
-    dagster_sling_translator=CustomSlingLowVolumeTranslator(),
+    dagster_sling_translator=CustomSlingTranslatorShard1(),
 )
 def cloud_product_shard1_low_volume(context, embedded_elt: SlingResource):
     yield from embedded_elt.replicate(context=context)
+
+
+@sling_assets(
+    replication_config=config_dir / "shard1_high_volume.yaml",
+    dagster_sling_translator=CustomSlingTranslatorShard1(),
+)
+def cloud_product_shard1_high_volume(context, embedded_elt: SlingResource):
+    yield from embedded_elt.replicate(context=context)
+
+
+cloud_product_shard1_source_assets = [
+    *[
+        SourceAsset(key, group_name="postgres_shard1")
+        for key in cloud_product_shard1_low_volume.dependency_keys
+    ],
+    *[
+        SourceAsset(key, group_name="postgres_shard1")
+        for key in cloud_product_shard1_high_volume.dependency_keys
+    ],
+]
 
 
 event_logs_freshness_checks = build_last_update_freshness_checks(
@@ -43,24 +101,3 @@ event_logs_freshness_checks = build_last_update_freshness_checks(
 freshness_checks_sensor = build_sensor_for_freshness_checks(
     freshness_checks=[event_logs_freshness_checks]
 )
-
-
-class CustomSlingHighVolumeTranslator(DagsterSlingTranslator):
-    def get_group_name(self, stream_definition):
-        return "cloud_product_high_volume_ingest"
-
-
-@sling_assets(
-    replication_config=config_dir / "main_high_volume.yaml",
-    dagster_sling_translator=CustomSlingHighVolumeTranslator(),
-)
-def cloud_product_main_high_volume(context, embedded_elt: SlingResource):
-    yield from embedded_elt.replicate(context=context)
-
-
-@sling_assets(
-    replication_config=config_dir / "shard1_high_volume.yaml",
-    dagster_sling_translator=CustomSlingHighVolumeTranslator(),
-)
-def cloud_product_shard1_high_volume(context, embedded_elt: SlingResource):
-    yield from embedded_elt.replicate(context=context)
