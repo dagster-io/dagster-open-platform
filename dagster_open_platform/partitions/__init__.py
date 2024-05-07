22abb7f5d8f0e2e3d014ffd73a9c175d3f168e1f
elementl-devtools <devtools@elementl.com>
2024-05-07 17:37:53 +0000

    Mirroring commit 480e0629f1b5bdc12ea76a1834133ff84f5efa89 from internal

--- dagster_open_platform/partitions/__init__.py
+++ dagster_open_platform/partitions/__init__.py
@@ -1,5 +1,14 @@
-from dagster import DailyPartitionsDefinition, WeeklyPartitionsDefinition
+from dagster import (
+    TimeWindowPartitionsDefinition,
+    WeeklyPartitionsDefinition,
+)
+from dagster._utils.partitions import DEFAULT_HOURLY_FORMAT_WITHOUT_TIMEZONE
 
-insights_partition = DailyPartitionsDefinition(start_date="2023-08-23")
+# split up every 3 hrs
+insights_partition = TimeWindowPartitionsDefinition(
+    start="2023-08-23-00:00",
+    cron_schedule="0 */3 * * *",
+    fmt=DEFAULT_HOURLY_FORMAT_WITHOUT_TIMEZONE,
+)
 
 oss_analytics_weekly_partition = WeeklyPartitionsDefinition(start_date="2019-01-01")
