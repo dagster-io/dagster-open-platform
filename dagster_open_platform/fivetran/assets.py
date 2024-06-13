821d8ab56c9d9821d50bb04728ee10de9705eddb
elementl-devtools <devtools@elementl.com>
2024-06-13 20:59:23 +0000

    Mirroring commit 1e83dab5707fa6e3fdfefcb9aa25197aa572bfc7 from internal

--- dagster_open_platform/fivetran/assets.py
+++ dagster_open_platform/fivetran/assets.py
@@ -1,17 +1,24 @@
-from dagster import AutoMaterializePolicy, AutoMaterializeRule, load_assets_from_current_module
+from dagster import (
+    AssetKey,
+    AutoMaterializePolicy,
+    AutoMaterializeRule,
+    EnvVar,
+    load_assets_from_current_module,
+)
+from dagster_fivetran import FivetranResource, load_assets_from_fivetran_instance
 
-# fivetran_instance = FivetranResource(
-#     api_key=EnvVar("FIVETRAN_API_KEY"),
-#     api_secret=EnvVar("FIVETRAN_API_SECRET"),
-# )
+fivetran_instance = FivetranResource(
+    api_key=EnvVar("FIVETRAN_API_KEY"),
+    api_secret=EnvVar("FIVETRAN_API_SECRET"),
+)
 
-# fivetran_assets_no_amp = load_assets_from_fivetran_instance(
-#     fivetran_instance,
-#     connector_to_group_fn=lambda connector: f"fivetran_{connector}",
-#     connector_to_asset_key_fn=lambda metadata, table_name: AssetKey(
-#         ["fivetran", *table_name.split(".")]
-#     ),
-# )
+fivetran_assets_no_amp = load_assets_from_fivetran_instance(
+    fivetran_instance,
+    connector_to_group_fn=lambda connector: f"fivetran_{connector}",
+    connector_to_asset_key_fn=lambda metadata, table_name: AssetKey(
+        ["fivetran", *table_name.split(".")]
+    ),
+)
 
 fivetran_assets = load_assets_from_current_module(
     auto_materialize_policy=AutoMaterializePolicy.eager().with_rules(
