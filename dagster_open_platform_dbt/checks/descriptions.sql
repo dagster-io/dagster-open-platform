01cb4762ed31fb1f9c74de01577fd1f8813905b0
elementl-devtools <devtools@elementl.com>
2024-08-23 02:29:12 +0000

    Mirroring commit ba81897512f11b187d8ea1d15336154bb14c93a7 from internal

--- dagster_open_platform_dbt/checks/descriptions.sql
+++ dagster_open_platform_dbt/checks/descriptions.sql
@@ -6,9 +6,11 @@ select
     materialization
 from sdf.information_schema.tables
 where 
-    catalog_name = 'PURINA'
-    and schema_name not like 'STAGING%'
-    and schema_name <> 'SNAPSHOTS'
-    and origin <> 'remote'
-    and materialization <> 'external-table'
-    and description is null
+    catalog_name = 'PURINA' -- Only look at purina tables
+    and schema_name not like 'STAGING%' -- Ignore staging tables
+    and schema_name <> 'SNAPSHOTS' -- Snapshot metadata not pulled in by sdf
+    and schema_name <> 'CLOUD_REPORTING' -- Ignore insights tables
+    and origin <> 'remote' -- Ignore remote tables
+    and materialization <> 'external-table' -- Ignore external tables
+    and (description is null or trim(description) = '') -- Find all tables without descriptions
+order by schema_name, table_name
