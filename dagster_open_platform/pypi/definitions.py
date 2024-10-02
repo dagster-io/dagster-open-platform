41a6069c32df8605540967835b538502cb92f579
elementl-devtools <devtools@elementl.com>
2024-10-02 17:16:33 +0000

    Mirroring commit 4f04c761bed1d005788f21fd0a357ae24b0cb0a9 from internal

--- dagster_open_platform/pypi/definitions.py
+++ dagster_open_platform/pypi/definitions.py
@@ -1,7 +1,6 @@
 from dagster import Definitions, load_assets_from_modules
 from dagster_open_platform.pypi import assets
 from dagster_open_platform.pypi.resources import bigquery_resource
-from dagster_open_platform.pypi.schedules import oss_analytics_schedule
 from dagster_open_platform.snowflake.resources import snowflake_resource
 
 from ..utils.source_code import add_code_references_and_link_to_git
@@ -10,6 +9,5 @@ pypi_assets = load_assets_from_modules([assets])
 
 defs = Definitions(
     assets=add_code_references_and_link_to_git(pypi_assets),
-    schedules=[oss_analytics_schedule],
     resources={"bigquery": bigquery_resource, "snowflake_pypi": snowflake_resource},
 )
