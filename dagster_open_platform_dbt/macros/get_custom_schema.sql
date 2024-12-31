0378da6e6ee27fe85f861d1f5659f6ca1b40c598
elementl-devtools <devtools@elementl.com>
2024-12-31 18:34:25 +0000

    Mirroring commit cc0bebbca113c9f30645f733645e2e3f2541c0ea from internal

--- dagster_open_platform_dbt/macros/get_custom_schema.sql
+++ dagster_open_platform_dbt/macros/get_custom_schema.sql
@@ -1,6 +1,6 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
     {%- set default_schema = target.schema -%}
-    {%- if target.name in ('prod', 'branch_deployment', 'dogfood') and custom_schema_name is none -%}
+    {%- if target.name in ('prod', 'branch_deployment', 'dogfood', 'sdf') and custom_schema_name is none -%}
         {% if node.fqn[1:-1]|length == 0 %}
             {{ default_schema }}    
         {% else %}
