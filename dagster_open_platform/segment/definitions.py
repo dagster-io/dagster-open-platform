1c46bd2026f53325262baaf7ae746b8c1c251743
elementl-devtools <devtools@elementl.com>
2025-01-13 15:30:58 +0000

    Mirroring commit 2a417e42df7416e44daf643ff3a993ba59605d36 from internal

--- dagster_open_platform/segment/definitions.py
+++ dagster_open_platform/segment/definitions.py
@@ -1,18 +1,34 @@
-from dagster import Definitions, SourceAsset
+from dagster import AssetSpec, Definitions
 
 from ..utils.source_code import add_code_references_and_link_to_git
 
 segment_asset_keys = [
     ["segment", "dagster_io", "identifies"],
     ["segment", "dagster_io", "pages"],
+    ["segment", "dagster_io", "hubspot_form_submit"],
+    ["segment", "dagster_cloud", "accounts"],
+    ["segment", "dagster_cloud", "identifies"],
+    ["segment", "dagster_cloud", "pages"],
     ["segment", "dagster_cloud", "tracks"],
+    ["segment", "dagster_cloud", "trial_start"],
+    ["segment", "dagster_cloud", "users"],
+    ["segment", "dagster_university", "certificate_record_downloaded"],
+    ["segment", "dagster_university", "course_landing_page"],
+    ["segment", "dagster_university", "identifies"],
+    ["segment", "dagster_university", "pages"],
+    ["segment", "dagster_university", "quiz_start_page"],
+    ["segment", "dagster_university", "signup"],
+    ["segment", "dagster_university", "tracks"],
+    ["segment", "dagster_university", "users"],
+    ["segment", "dagster_university", "viewed_iframe"],
 ]
 
 segment_source_assets = [
-    SourceAsset(
+    AssetSpec(
         key=key,
         description="A table containing Segment data loaded using a Snowflake integration.",
         group_name="segment",
+        kinds={"segment"},
     )
     for key in segment_asset_keys
 ]
