82a2cc47ded5f906fc533806ec654d98f53a0288
elementl-devtools <devtools@elementl.com>
2025-03-10 13:42:53 +0000

    Mirroring commit e598339b84781455f77ffb0a16d7d353d4e13275 from internal

--- dagster_open_platform_dbt/snapshots/purina/snapshots/hubspot/hubspot_company_daily_snapshot.sql
+++ dagster_open_platform_dbt/snapshots/purina/snapshots/hubspot/hubspot_company_daily_snapshot.sql
@@ -17,9 +17,7 @@
             in America/Los_Angeles timezone. We subtract 1 day because it
             represents the ending values of the previous day.
         */
-        c1.*,
-        c2.intent_score
+        c1.*
     from {{ ref('stg_hubspot__company') }} c1
-        left join {{ ref('abm_company_intent_scores') }} c2 on c1.id = c2.hubspot_company_id
 
 {% endsnapshot %}
