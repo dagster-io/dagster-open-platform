4ffd566131eaa3083836a2bc7e4623892802b50a
elementl-devtools <devtools@elementl.com>
2025-03-20 15:08:35 +0000

    Mirroring commit 272993d6a6240c52179784cbef3cd4f84e73d316 from internal

--- dagster_open_platform_dbt/snapshots/purina/snapshots/salesforce/salesforce_opportunities_daily_snapshot.sql
+++ dagster_open_platform_dbt/snapshots/purina/snapshots/salesforce/salesforce_opportunities_daily_snapshot.sql
@@ -12,6 +12,7 @@
     select
         convert_timezone('UTC', 'America/Los_Angeles', current_timestamp)::date
         - interval '1 day' as snapshot_date,
+        current_timestamp as snapshot_at,
         /*
             This will generate a new snapshot on the first run of the day
             in America/Los_Angeles timezone. We subtract 1 day because it
