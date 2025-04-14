449b99f01bb29b46aba387cde44dd8968d79f57f
elementl-devtools <devtools@elementl.com>
2025-04-14 19:11:50 +0000

    Mirroring commit 52a560598d9fa187e47882986bcceb616b468c58 from internal

--- dagster_open_platform_dbt/macros/get_custom_database.sql
+++ dagster_open_platform_dbt/macros/get_custom_database.sql
@@ -1,12 +1,12 @@
 {% macro generate_database_name(custom_database_name=none, node=none) -%}
     {%- set default_database = target.database -%}
-    {%- if target.name in ('prod', 'branch_deployment', 'dogfood', 'sdf', 'dev', 'ci') and custom_database_name is none -%}
+    {%- if target.name in ('prod', 'branch_deployment', 'dogfood', 'sdf', 'dev', 'purina_ci') and custom_database_name is none -%}
         {% if node.fqn|length <= 2 %}
             {{ default_database }}
         {% elif target.name == 'branch_deployment' %}
             {% set prefix = node.fqn[1] %}
             {{ return([prefix | trim, 'clone', env_var('DAGSTER_CLOUD_PULL_REQUEST_ID')]|join('_')) }}
-        {% elif target.name == 'ci' %}
+        {% elif target.name == 'purina_ci' %}
             {% set prefix = node.fqn[1] %}
             {{ return([prefix | trim, 'ci']|join('_')) }}
         {% else %}
