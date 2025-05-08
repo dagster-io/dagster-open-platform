fb9fee5331be3544f7f4409dfb7eeb8c427a36e9
elementl-devtools <devtools@elementl.com>
2025-05-08 20:58:35 +0000

    Mirroring commit 66cde8cd01196c4a2f65897b61a616abb5b0fd5d from internal

--- dagster_open_platform/defs/hightouch/schedules.py
+++ dagster_open_platform/defs/hightouch/schedules.py
@@ -14,22 +14,29 @@ hightouch_syncs_schedule = dg.ScheduleDefinition(
     cron_schedule="0 3 * * *",
 )
 
-hightouch_hubspot_syncs_schedule = dg.ScheduleDefinition(
-    name="hightouch_hubspot_syncs_schedule",
-    job=dg.define_asset_job(
-        name="hightouch_hubspot_syncs_job",
-        partition_key=insights_partition.get_last_partition_key(),
-        selection=(
-            dg.AssetSelection.keys(
-                "hightouch_sync_hubspot_company", "hightouch_sync_hubspot_organization"
-            )
-            .upstream()
-            .required_multi_asset_neighbors()
-            - dg.AssetSelection.groups("cloud_product_main").upstream()
-            - dg.AssetSelection.groups("cloud_product_shard1").upstream()
-            - dg.AssetSelection.groups("staging_aws").upstream()
-        ),
+hightouch_hubspot_syncs_job = dg.define_asset_job(
+    name="hightouch_hubspot_syncs_job",
+    partitions_def=insights_partition,
+    selection=(
+        dg.AssetSelection.keys(
+            "hightouch_sync_hubspot_company", "hightouch_sync_hubspot_organization"
+        )
+        .upstream()
+        .required_multi_asset_neighbors()
+        - dg.AssetSelection.groups("cloud_product_main").upstream()
+        - dg.AssetSelection.groups("cloud_product_shard1").upstream()
+        - dg.AssetSelection.groups("staging_aws").upstream()
     ),
-    tags={"team": "devrel", "dagster/max_retries": 1},
-    cron_schedule="0 * * * *",
 )
+
+
+@dg.schedule(
+    cron_schedule="0 * * * *",
+    job=hightouch_hubspot_syncs_job,
+    tags={"team": "devrel", "dagster/max_retries": 1},
+)
+def hightouch_hubspot_syncs_schedule():
+    most_recent_partition = insights_partition.get_last_partition_key()
+    yield dg.RunRequest(
+        partition_key=str(most_recent_partition), run_key=str(most_recent_partition)
+    )
