59c88d1a3aa6d82c0fbb9c9099e476a2a28748b2
elementl-devtools <devtools@elementl.com>
2025-05-17 00:01:56 +0000

    Mirroring commit 6b9dcbad666fe45b03482602e4d20f4438d01366 from internal

--- dagster_open_platform/defs/fivetran/assets.py
+++ dagster_open_platform/defs/fivetran/assets.py
@@ -1,20 +1,35 @@
 import dagster as dg
+from dagster.components import definitions
 from dagster_fivetran import FivetranResource, load_assets_from_fivetran_instance
 
-fivetran_instance = FivetranResource(
-    api_key=dg.EnvVar("FIVETRAN_API_KEY"),
-    api_secret=dg.EnvVar("FIVETRAN_API_SECRET"),
-)
 
-fivetran_assets_no_amp = load_assets_from_fivetran_instance(
-    fivetran_instance,
-    connector_to_group_fn=lambda connector: f"fivetran_{'_'.join(connector.split('.'))}",
-    connector_to_asset_key_fn=lambda metadata, table_name: dg.AssetKey(
-        ["fivetran", *table_name.split(".")]
-    ),
-)
+@definitions
+def defs():
+    fivetran_instance = FivetranResource(
+        api_key=dg.EnvVar("FIVETRAN_API_KEY"),
+        api_secret=dg.EnvVar("FIVETRAN_API_SECRET"),
+    )
 
-fivetran_assets = dg.load_assets_from_current_module(
-    automation_condition=dg.AutomationCondition.cron_tick_passed("0 * * * *")
-    & ~dg.AutomationCondition.in_progress(),
-)
+    fivetran_assets_no_amp = load_assets_from_fivetran_instance(
+        fivetran_instance,
+        connector_to_group_fn=lambda connector: f"fivetran_{'_'.join(connector.split('.'))}",
+        connector_to_asset_key_fn=lambda metadata, table_name: dg.AssetKey(
+            ["fivetran", *table_name.split(".")]
+        ),
+    )
+
+    fivetran_assets = fivetran_assets_no_amp.with_attributes_for_all(
+        group_name=None,
+        freshness_policy=None,
+        backfill_policy=None,
+        auto_materialize_policy=(
+            dg.AutomationCondition.cron_tick_passed("0 * * * *")
+            & ~dg.AutomationCondition.in_progress()
+        ).as_auto_materialize_policy(),
+    )
+
+    return dg.Definitions(
+        assets=[
+            fivetran_assets,
+        ]
+    )
--- dagster_open_platform/defs/fivetran/checks.py
+++ dagster_open_platform/defs/fivetran/checks.py
@@ -1,15 +1,16 @@
-from dagster import AssetCheckResult, asset_check
+from dagster import AssetCheckResult, Definitions, asset_check
+from dagster.components import definitions
 from dagster_snowflake import SnowflakeResource
 
 
 @asset_check(asset=["fivetran", "salesforce", "account"])
-def account_has_valid_org_id(snowflake_fivetran: SnowflakeResource) -> AssetCheckResult:
-    with snowflake_fivetran.get_connection() as conn:
+def account_has_valid_org_id(snowflake_sf: SnowflakeResource) -> AssetCheckResult:
+    with snowflake_sf.get_connection() as conn:
         cur = conn.cursor()
         cur.execute("""
             select organization_id_c as org_id
             from fivetran.salesforce.account
-            where organization_id_c not regexp '\\\\d+' and not is_deleted           
+            where organization_id_c not regexp '\\\\d+' and not is_deleted
         """)
         result = cur.fetch_pandas_all()
     return AssetCheckResult(
@@ -17,4 +18,11 @@ def account_has_valid_org_id(snowflake_fivetran: SnowflakeResource) -> AssetChec
         metadata={
             "org_ids": result["ORG_ID"].to_list(),
         },
+    )
+
+
+@definitions
+def defs():
+    return Definitions(
+        asset_checks=[account_has_valid_org_id],
     )
