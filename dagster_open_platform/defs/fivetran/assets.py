de70701bf7246c5343663be04e83fe578bf5cd8e
elementl-devtools <devtools@elementl.com>
2025-05-20 21:05:57 +0000

    Mirroring commit 4ccfa46c7117e307c203065348666a41692db2f8 from internal

--- dagster_open_platform/defs/fivetran/assets.py
+++ dagster_open_platform/defs/fivetran/assets.py
@@ -1,35 +1,37 @@
 import dagster as dg
 from dagster.components import definitions
-from dagster_fivetran import FivetranResource, load_assets_from_fivetran_instance
+from dagster_fivetran import (
+    DagsterFivetranTranslator,
+    FivetranConnectorTableProps,
+    FivetranWorkspace,
+    build_fivetran_assets_definitions,
+)
+
+
+class DOPFivetranTranslator(DagsterFivetranTranslator):
+    def get_asset_spec(self, props: FivetranConnectorTableProps) -> dg.AssetSpec:
+        return (
+            super()
+            .get_asset_spec(props)
+            .replace_attributes(
+                key=dg.AssetKey(["fivetran", *props.table.split(".")]),
+                automation_condition=dg.AutomationCondition.cron_tick_passed("0 * * * *")
+                & ~dg.AutomationCondition.in_progress(),
+                group_name=f"fivetran_{'_'.join(props.table.split('.')[:-1])}",
+            )
+        )
 
 
 @definitions
 def defs():
-    fivetran_instance = FivetranResource(
+    fivetran_instance = FivetranWorkspace(
+        account_id=dg.EnvVar("FIVETRAN_ACCOUNT_ID"),
         api_key=dg.EnvVar("FIVETRAN_API_KEY"),
         api_secret=dg.EnvVar("FIVETRAN_API_SECRET"),
     )
 
-    fivetran_assets_no_amp = load_assets_from_fivetran_instance(
-        fivetran_instance,
-        connector_to_group_fn=lambda connector: f"fivetran_{'_'.join(connector.split('.'))}",
-        connector_to_asset_key_fn=lambda metadata, table_name: dg.AssetKey(
-            ["fivetran", *table_name.split(".")]
-        ),
+    fivetran_assets = build_fivetran_assets_definitions(
+        workspace=fivetran_instance, dagster_fivetran_translator=DOPFivetranTranslator()
     )
 
-    fivetran_assets = fivetran_assets_no_amp.with_attributes_for_all(
-        group_name=None,
-        freshness_policy=None,
-        backfill_policy=None,
-        auto_materialize_policy=(
-            dg.AutomationCondition.cron_tick_passed("0 * * * *")
-            & ~dg.AutomationCondition.in_progress()
-        ).as_auto_materialize_policy(),
-    )
-
-    return dg.Definitions(
-        assets=[
-            fivetran_assets,
-        ]
-    )
+    return dg.Definitions(assets=fivetran_assets, resources={"fivetran": fivetran_instance})
