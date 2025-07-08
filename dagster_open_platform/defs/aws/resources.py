26c5cceba9b4e344054812427ce90ea94b660abd
elementl-devtools <devtools@elementl.com>
2025-07-08 21:32:06 +0000

    Mirroring commit 90c0f57bbd054238e436b3b9222cddbc79fc2629 from internal

--- dagster_open_platform/defs/aws/resources.py
+++ dagster_open_platform/defs/aws/resources.py
@@ -1,5 +1,6 @@
 import os
 
+import boto3
 from dagster import Definitions
 from dagster.components import definitions
 from dagster_aws.s3 import S3Resource
@@ -7,12 +8,21 @@ from dagster_aws.s3 import S3Resource
 
 @definitions
 def defs():
+    region = os.getenv("AWS_WORKSPACE_REPLICATION_REGION")
+    role_arn = os.getenv("AWS_WORKSPACE_REPLICATION_ROLE_ARN")
+    creds = boto3.client("sts").assume_role(
+        RoleArn=role_arn,
+        RoleSessionName="dagster-workspace-replication",
+        DurationSeconds=3600,
+    )["Credentials"]
+
     return Definitions(
         resources={
             "s3_resource": S3Resource(
-                aws_access_key_id=os.getenv("AWS_WORKSPACE_REPLICATION_ACCESS_KEY_ID"),
-                aws_secret_access_key=os.getenv("AWS_WORKSPACE_REPLICATION_SECRET_ACCESS_KEY"),
-                region_name=os.getenv("AWS_WORKSPACE_REPLICATION_REGION"),
+                region_name=region,
+                aws_access_key_id=creds["AccessKeyId"],
+                aws_secret_access_key=creds["SecretAccessKey"],
+                aws_session_token=creds["SessionToken"],
             )
         }
     )
