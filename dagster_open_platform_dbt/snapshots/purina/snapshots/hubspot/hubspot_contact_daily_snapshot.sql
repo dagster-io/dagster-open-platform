8e3e5895df0bbc06ceb2b66bd24fabd19599a0e9
elementl-devtools <devtools@elementl.com>
2025-07-17 19:06:25 +0000

    Mirroring commit 10334841f3ab471268ee3294f72a47926474a984 from internal

--- dagster_open_platform_dbt/snapshots/purina/snapshots/hubspot/hubspot_contact_daily_snapshot.sql
+++ dagster_open_platform_dbt/snapshots/purina/snapshots/hubspot/hubspot_contact_daily_snapshot.sql
@@ -18,43 +18,8 @@
             in America/Los_Angeles timezone. We subtract 1 day because it
             represents the ending values of the previous day.
         */
-        contact_id,
-        email,
-        email_domain,
-        first_name,
-        last_name,
-        associatedcompanyid,
-        organization_id,
-        company,
-        lead_source,
-        trial_source,
-        marketing_lead_source,
-        latest_source,
-        lead_source_action,
-        latest_source_drill_level_1,
-        latest_source_drill_level_2,
-        latest_source_date,
-        original_source,
-        original_source_drill_level_1,
-        original_source_drill_level_2,
-        import_source,
-        lifecycle_stage,
-        lifecycle_stage_date,
-        hubspot_score,
-        time_in_mql,
-        lifecycle_stage_mql_date,
-        predictive_scoring_tier,
-        mql_date,
-        is_mql,
-        mql_status,
-        predictive_contact_score,
-        salesforce_contact_id,
-        create_time,
-        create_date,
-        request_type,
-        how_did_you_hear_about_us,
-        message,
-        abm_score
+        c1.*,
+        c2.abm_score
     from {{ ref('stg_hubspot__contacts') }} c1
         left join {{ ref('abm_contact_intent_scores') }} c2 on c1.contact_id = c2.hubspot_contact_id
 
