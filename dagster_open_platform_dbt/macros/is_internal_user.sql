612e5af688c71ac99f2cf498829da4a3ea59267a
elementl-devtools <devtools@elementl.com>
2025-08-12 16:40:39 +0000

    Mirroring commit 899a83bae810dbb086e814b07fd47565ea5c5376 from internal

--- dagster_open_platform_dbt/macros/is_internal_user.sql
+++ dagster_open_platform_dbt/macros/is_internal_user.sql
@@ -1,8 +1,13 @@
+--- Please note the following specific email addresses that are internal users:
+--- pedram@pedramnavid.com = Pedram's personal email
+--- justin@novotta.com = Contract developer that works on our website
+
 {% macro is_internal_user(email) -%}
 
     case 
-        when {{email}} like '%@elementl.%' then true
-        when {{email}} like '%@dagsterlabs.%' then true
+        when {{email}} ilike any ('%@elementl.%', '%@dagsterlabs.%', '%@dagster.io', '%@dagster.info') then true
+        when {{email}} = 'pedram@pedramnavid.com' then true
+        when {{email}} = 'justin@novotta.com' then true
         else false
     end
 
