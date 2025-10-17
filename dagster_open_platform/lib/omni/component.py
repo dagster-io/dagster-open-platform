f315498ce838f0e52b58bdfb38c5124b7e967980
elementl-devtools <devtools@elementl.com>
2025-10-17 21:04:48 +0000

    Mirroring commit 3150cd248e69bc2460232ca07e03a8cfd2c0d216 from internal

--- dagster_open_platform/lib/omni/component.py
+++ dagster_open_platform/lib/omni/component.py
@@ -3,7 +3,7 @@ from typing import Optional
 import dagster as dg
 from dagster_dbt import get_asset_key_for_model
 from dagster_omni import OmniComponent
-from dagster_omni.objects import OmniQuery
+from dagster_omni.objects import OmniDocument, OmniQuery
 from dagster_omni.translation import OmniTranslatorData
 
 
@@ -16,15 +16,18 @@ class DOPOmniComponent(OmniComponent):
         from dagster_open_platform.defs.dbt.assets import get_dbt_non_partitioned_models
 
         base_spec = super().get_asset_spec(context, data)
-
-        # Filter out assets without a group name or with "archived" group
-        if base_spec and (
-            base_spec.group_name is None or base_spec.group_name in ["archive", "playground"]
-        ):
+        if not base_spec:
             return None
 
-        if base_spec and isinstance(data.obj, OmniQuery):
-            # attempt to map the key to a dbt model
+        # filter out documents that are in the archive or playground groups
+        if isinstance(data.obj, OmniDocument) and base_spec.group_name in [
+            None,
+            "archive",
+            "playground",
+        ]:
+            return None
+        elif isinstance(data.obj, OmniQuery):
+            # attempt to map the query key to a dbt model
             try:
                 key = get_asset_key_for_model(
                     [get_dbt_non_partitioned_models()],
@@ -34,4 +37,5 @@ class DOPOmniComponent(OmniComponent):
             # if no matching dbt model, return the original spec
             except KeyError:
                 pass
+
         return base_spec
