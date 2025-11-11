8caa5e917f0e819f5be9ea63dea2c39d160b7472
elementl-devtools <devtools@elementl.com>
2025-11-11 20:28:40 +0000

    Mirroring commit f707c1f5ad33e1266c7caa8cbeb3c210f38e8bf7 from internal

--- dagster_open_platform/defs/datadog/py/schedule.py
+++ dagster_open_platform/defs/datadog/py/schedule.py
@@ -6,7 +6,7 @@ from dagster.components import definitions
 def defs() -> dg.Definitions:
     datadog_usage_job = dg.define_asset_job(
         "datadog_usage_job",
-        selection=["datadog/raw/usage"],
+        selection=["datadog/raw/usage", "datadog/raw/estimated_usage"],
         partitions_def=dg.MonthlyPartitionsDefinition(start_date="2025-01-01"),
     )
     return dg.Definitions(
