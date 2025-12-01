99a15fbe874f441486417d04b08c4e1faa9cf294
elementl-devtools <devtools@elementl.com>
2025-12-01 15:03:18 +0000

    Mirroring commit 1161afde2d03b51399df4ebff830b71f8a089679 from internal

--- dagster_open_platform/defs/dbt/schedules.py
+++ dagster_open_platform/defs/dbt/schedules.py
@@ -39,13 +39,17 @@ def defs():
             partition_key=str(most_recent_partition), run_key=str(most_recent_partition)
         )
 
-    dbt_analytics_snapshot_schedule = dg.ScheduleDefinition(
-        job=dg.define_asset_job(
-            name="dbt_analytics_snapshot_job",
-            selection=dg.AssetSelection.assets(get_dbt_snapshot_models()).downstream()
-            - dg.AssetSelection.kind("omni", include_sources=True),
-        ),
-        cron_schedule="0 7 * * *",
+    dbt_analytics_snapshot_job = dg.define_asset_job(
+        name="dbt_analytics_snapshot_job",
+        selection=dg.AssetSelection.assets(get_dbt_snapshot_models()).downstream()
+        - dg.AssetSelection.kind("omni", include_sources=True),
     )
+
+    @dg.schedule(cron_schedule="0 7 * * *", job=dbt_analytics_snapshot_job)
+    def dbt_analytics_snapshot_schedule():
+        most_recent_partition = insights_partition.get_last_partition_key()
+        yield dg.RunRequest(
+            partition_key=str(most_recent_partition), run_key=str(most_recent_partition)
+        )
 
     return dg.Definitions(schedules=[dbt_analytics_core_schedule, dbt_analytics_snapshot_schedule])
